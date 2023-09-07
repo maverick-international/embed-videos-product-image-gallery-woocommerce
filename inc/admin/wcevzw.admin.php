c03c455a40a45db4f9f6f8bbf61e1825aeef00cd
Dan Devine <dandevine0@gmail.com>
2023-09-07 12:48:19 +0100

    remove image duplication

---
+++ inc/admin/wcevzw.admin.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ *
+ * Handles the admin functionality.
+ *
+ * @package WordPress
+ * @subpackage Embed Videos For Product Image Gallery Using WooCommerce
+ * @since 1.0
+ */
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Check if WooCommerce is active
+ */
+require_once ( WCEVZW_FILE );
+global $post;
+
+register_activation_hook ( WCEVZW_FILE, 'wcevzw_woo_activation_check');
+function wcevzw_woo_activation_check()
+{
+	if( !is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
+		deactivate_plugins( WCEVZW_PLUGIN_BASENAME );
+		wp_die( _e( '<b>Warning</b> : Install/Activate Woocommerce to activate "Embed Videos For Product Image Gallery Using WooCommerce" plugin.' , 'embed-videos-product-image-gallery-woocommerce' ) );
+	}
+}
+
+/**
+ * Add settings link to plugins page
+ */
+add_filter( 'plugin_action_links_' . WCEVZW_PLUGIN_BASENAME, 'wcevzw_add_action_links_embed_video' );
+function wcevzw_add_action_links_embed_video ( $links ) {
+	 $settingslinks = array(
+	 '<a href="' . admin_url( 'admin.php?page=embed-videos-settings' ) . '"> '. __( 'Settings', 'embed-videos-product-image-gallery-woocommerce') .'</a>',
+	 );
+	return array_merge( $settingslinks, $links );
+}
+
+/**
+ * Set up menu under woocommerce
+ */
+add_action( 'admin_menu', 'wcevzw_embed_videos_setup_menu' );
+function wcevzw_embed_videos_setup_menu() {
+	add_submenu_page( 'woocommerce', 'Embed Videos To Product Image Gallery', 'Embed Videos Settings', 'manage_options', 'embed-videos-settings', 'embed_videos_init');
+}
+
+/**
+ * Register options of this plugin
+ */
+add_action( 'admin_init', 'wcevzw_register_embed_videos_settings' );
+function wcevzw_register_embed_videos_settings() {
+	register_setting( 'embed-videos-settings', 'embed_videos_autoplay' );
+	register_setting( 'embed-videos-settings', 'embed_videos_rel' );
+	register_setting( 'embed-videos-settings', 'embed_videos_showinfo' );
+	register_setting( 'embed-videos-settings', 'embed_videos_disablekb' );
+	register_setting( 'embed-videos-settings', 'embed_videos_fs' );
+	register_setting( 'embed-videos-settings', 'embed_videos_controls' );
+	register_setting( 'embed-videos-settings', 'embed_videos_hd' );
+ }
+
+
+/**
+* Initialize the plugin and display all options at admin side
+*/
+function embed_videos_init() {
+?>
+  <h1><?php echo _e( 'Youtube Video Settings', 'embed-videos-product-image-gallery-woocommerce' ); ?></h1>
+  <form method="post" action="options.php">
+	<?php settings_fields( 'embed-videos-settings' ); ?>
+	<?php do_settings_sections( 'embed-videos-settings' ); ?>
+	<table class="form-table">
+		<tr valign="top">
+			<th scope="row"><?php echo _e( 'Autoplay videos', 'embed-videos-product-image-gallery-woocommerce' ).':'; ?></th>
+			<td><input type="checkbox" name="embed_videos_autoplay" value="1" <?php echo ( get_option( 'embed_videos_autoplay' ) == 1 ) ? 'checked': ''; ?> /></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row"><?php echo _e( 'Show relative videos', 'embed-videos-product-image-gallery-woocommerce' ).':'; ?></th>
+			<td><input type="checkbox" name="embed_videos_rel" value="1" <?php echo ( get_option( 'embed_videos_rel' ) == 1 ) ? 'checked': ''; ?> /></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row"><?php echo _e( 'Show video information', 'embed-videos-product-image-gallery-woocommerce' ).':'; ?></th>
+			<td>
+				<input type="checkbox" name="embed_videos_showinfo" value="1" <?php echo ( get_option( 'embed_videos_showinfo' ) == 1 ) ? 'checked': ''; ?> />
+			</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row"><?php echo _e( 'Show fullscreen button', 'embed-videos-product-image-gallery-woocommerce' ).':'; ?></th>
+			<td>
+				<input type="checkbox" name="embed_videos_fs" value="1" <?php echo ( get_option( 'embed_videos_fs' ) == 1 ) ? 'checked': ''; ?> />
+			</td>
+		</tr>
+		 <tr valign="top">
+			<th scope="row"><?php echo _e( 'Show video player controls', 'embed-videos-product-image-gallery-woocommerce' ).':'; ?></th>
+			<td>
+				<input type="checkbox" name="embed_videos_controls" value="1" <?php echo ( get_option( 'embed_videos_controls' ) == 1 ) ? 'checked': ''; ?> />
+			</td>
+		</tr>
+	</table>
+	<?php submit_button(); ?>
+	</div>
+  </form>
+
+<?php
+}
+
+/**
+ * Add form field to get video link id for product image
+ */
+add_filter( 'attachment_fields_to_edit', 'wcevzw_woo_embed_video', 20, 2);
+function wcevzw_woo_embed_video( $form_fields, $attachment ) {
+
+	$url = $_SERVER['HTTP_REFERER'];
+	parse_str(parse_url($url)['query'], $params);
+	$post_id=$params['post']; //
+
+	if (! $post_id = (int) ($post_id ?? null)) {
+		return $form_fields;
+	}
+
+	$nonce = wp_create_nonce( 'bdn-attach_' . $attachment->ID );
+	$attach_image_action_url = admin_url( "media-upload.php?tab=library&post_id=$post_id" );
+
+	$field_value = get_post_meta( $attachment->ID, 'videolink_id', true );
+	$video_site = get_post_meta( $attachment->ID, 'video_site', true );
+	$youtube = ( $video_site == 'youtube' ) ? 'checked' : '';
+	$vimeo = ( $video_site == 'vimeo' ) ? 'checked' : '';
+	$checked = '';
+	if( empty( $youtube ) && empty( $vimeo) )
+	{
+		$checked = 'checked';
+	}
+	$form_fields['videolink_id'] = array(
+		'value' => $field_value ? $field_value : '',
+		'input' => "text",
+		'label' => __( 'Video Link ID', 'embed-videos-product-image-gallery-woocommerce' )
+	);
+	$form_fields['video_site'] = array(
+		'input' => 'html',
+		'value' => $video_site,
+		'html' => "<input type='radio' name='attachments[{$attachment->ID}][video_site]' value='youtube' $youtube $checked> Youtube
+					<input type='radio' name='attachments[{$attachment->ID}][video_site]' value='vimeo' $vimeo> Vimeo",
+		'helps' => __( '<b>For Eg.:</b> <br>"112233445" for URL - https://vimeo.com/112233445 <br>
+					 <br>"n93gYncUD" for URL - https://www.youtube.com/watch?v=n93gYncUD' )
+	);
+	return $form_fields;
+}
+
+/**
+* Save form field of video link to display video on product image
+*/
+add_action( 'edit_attachment', 'wcevzw_woo_save_embed_video' );
+function wcevzw_woo_save_embed_video( $attachment_id ) {
+	if ( isset( $_REQUEST['attachments'][$attachment_id]['videolink_id'] ) ) {
+		$videolink_id = sanitize_text_field ( $_REQUEST['attachments'][$attachment_id]['videolink_id'] );
+		update_post_meta( $attachment_id, 'videolink_id', $videolink_id );
+	}
+	if ( isset( $_REQUEST['attachments'][$attachment_id]['video_site'] ) ) {
+		$video_site = sanitize_text_field ( $_REQUEST['attachments'][$attachment_id]['video_site'] );
+		update_post_meta( $attachment_id, 'video_site', $video_site );
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,128 @@
+=== Embed Videos For Product Image Gallery Using WooCommerce ===
+
+Contributors: zealopensource
+Tags: woocommerce, gallery, video gallery, video and image gallery, embed videos, videos woocommerce, iframe, youtube videos, vimeo videos, videos product gallery
+Donate link: http://www.zealousweb.com/payment/
+Requires at least: 3.0.1
+Tested up to: 6.3
+Requires PHP: 5.3
+Stable tag: 3.2
+License: GPLv3 or later License
+Version: 3.2
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Embed videos to product gallery along with images on product page of WooCommerce.
+
+== Description ==
+
+Embed videos to product gallery along with images on product page of WooCommerce.
+
+= Embed Videos For Product Image Gallery Using WooCommerce Features =
+
+*	Embed videos to product image gallery
+*	Supports YouTube and Vimeo videos
+*	Video loads as inline embedded videos in place of featured image or in a lightbox based on WooCommerce settings
+*	Allows images and videos together in single product gallery
+*	Does not require to overwrite any WooCommerce core files
+*	Youtube video control parameters are supported.
+*	Compatibility with multisite network.
+
+== Plugin Requirement ==
+
+PHP version : 5.3 and latest
+WordPress   : Wordpress 3.0 and latest
+
+== OUR OTHER PLUGINS ==
+
+* <strong>[Accept PayPal Payments Using Contact Form 7 Pro](https://www.zealousweb.com/store/accept-paypal-payments-using-contact-form-7-pro)</strong>
+* <strong>[Accept Stripe Payments Using Contact Form 7 Pro](https://www.zealousweb.com/store/accept-stripe-payments-using-contact-form-7-pro)</strong>
+* <strong>[Accept Authorize.NET Payments Using Contact Form 7 Pro](https://www.zealousweb.com/store/accept-authorize-net-payments-using-contact-form-7-pro)</strong>
+* <strong>[Accept Elavon Payments Using Contact Form 7 Pro](https://www.zealousweb.com/store/accept-elavon-payments-using-contact-form-7-pro)</strong>
+* <strong>[Accept 2Checkout Payments Using Contact Form 7 Pro](https://www.zealousweb.com/store/accept-2checkout-payments-using-contact-form-7-pro)</strong>
+* <strong>[Accept SagePay(opayo) Payments Using Contact Form 7 Pro](https://www.zealousweb.com/store/accept-sage-pay-payments-using-contact-form-7-pro)</strong>
+* <strong>[User Registration Using Contact Form 7 Pro](https://www.zealousweb.com/store/user-registration-using-contact-form-7-pro)</strong>
+* <strong>[Abandoned Contact Form 7 Pro](https://www.zealousweb.com/store/abandoned-contact-form-7-pro/)</strong>
+* <strong>[Custom Product Options WooCommerce Pro](https://www.zealousweb.com/store/custom-product-options-woocommerce-pro)</strong>
+
+== Installation ==
+1. Install "Embed Videos For Product Image Gallery Using WooCommerce" either via the WordPress.org plugin directory, or by uploading the files to your server.
+2. Activate the plugin in your Wordpress Admin and go to Products at admin panel.
+3. Edit any product to add Video Link ID in Product Gallery media tool.
+4. Use ID of video For Eg.: "112233445" for URL - https://vimeo.com/112233445 & "n93gYncUD" for URL - https://www.youtube.com/watch?v=n93gYncUD.
+5. Enjoy videos in product gallery at frontend.
+
+== Screenshots ==
+1. shows all the possible options available for this plugin at Product Gallery media tool.
+2. shows front end view of inline embedded videos.
+3. shows front end view of videos in lighbox popup.
+4. shows settings page of Youtube video parameters.
+
+== Frequently Asked Questions ==
+
+= Installation Instructions =
+* Install "Embed Videos For Product Image Gallery Using WooCommerce" either via the WordPress.org plugin directory, or by uploading the files to your server.
+* Activate the plugin in your WordPress Admin and go to Products at admin panel.
+* Edit any product to add Video Link ID in Product Gallery media tool.
+* Use ID of video For Eg.: “112233445” for URL – https://vimeo.com/112233445 & “n93gYncUD” for URL – https://www.youtube.com/watch?v=n93gYncUD.
+* Enjoy videos in product gallery at frontend.
+
+== Changelog ==
+
+= 3.2 =
+* Fix - Fixed the product media gallery image not showing issue when added video on it.
+
+= 3.1 =
+* Fix - Bug fixed on Product media gallery
+
+= 3.0 =
+* Fix - Make Multi-Site Compatibility and update syntax of text domain.
+
+= 2.9 =
+* Test - Checking with Latest wordPress and Contact form 7, Tested and works fine.
+
+= 2.8 - 27/10/2020 =
+* Fix - Solved Gallery Photoswip issue.
+
+= 2.7 - 21/08/2020 =
+* Change - Update Documents and author name.
+
+= 2.6 - 29/05/2020 =
+* Fix - Bug fixing.
+
+= 2.5 - 25/05/2020 =
+* Fix - Bug fixing.
+
+= 2.4 - 26/03/2019 =
+* Change - Update Plugin Name.
+
+= 2.3 - 18/07/2017 =
+* Fix - Bug fixing.
+
+= 2.2 - 18/07/2017 =
+* Change - Bug fixing.
+
+= 2.1 - 10/07/2017 =
+* Change - We have make plugin comparable to latest woo commerce 3.1.0 with wordpress 4.8.
+
+= 2.0 - 18/04/2017 =
+* Change - We have make plugin comparable to latest woo commerce 3.0.3.
+
+= 1.2 - 02/09/2015 =
+* Change - Added Youtube video control Settings.
+
+= 1.1 - 27/04/2015 =
+* Change - Fixed some issues.
+
+= 1.0 =
+* Initial Release
+
+== Upgrade Notice ==
+
+= 2.4 =
+* Change - Update Plugin Name.
+
+= 2.3 =
+Bug fixing and remove unwanted code.
+
+= 2.4 =
+* Change - Update Plugin Name.
